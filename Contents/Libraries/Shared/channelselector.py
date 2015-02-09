9a7fb878f09715c42604998d6f76d8a13864aebe
Fernando San Julián <fjsanjulian@hotmail.com>
2015-02-09 14:11:30 +0100

    added pornhub channel

--- Contents/Libraries/Shared/channelselector.py
+++ Contents/Libraries/Shared/channelselector.py
@@ -335,6 +335,7 @@ def channels_list():
     itemlist.append( Item( title="Pirate Streaming (IT)" , channel="piratestreaming"      , language="IT" , category="F" , type="generic"    )) # jesus 16/7/2012
     itemlist.append( Item( title="Playmax"               , channel="playmax"      , language="ES" , category="F,S" , type="generic"    )) # jesus 16/7/2012
     itemlist.append( Item( title="Pordede"               , channel="pordede"              , language="ES" , category="F,S" , type="generic"    )) # jesus 16/6/2014
+    if config.get_setting("enableadultmode") == "true": itemlist.append( Item( title="pornhub"          , channel="pornhub"             , language="ES" , category="F" , type="generic"  ))
     #itemlist.append( Item( title="PelisFlv"              , channel="pelisflv"             , language="ES" , category="F"          , type="xbmc"  ))
     itemlist.append( Item( title="Quebajamos"            , channel="quebajamos", language="ES" , category="F,S,D" , type="generic"  )) # jesus 16/06/2014
     itemlist.append( Item( title="Quiero Dibujos Animados", channel="quierodibujosanimados", language="ES" , category="S" , type="generic"  )) # jesus 12/11/2012
